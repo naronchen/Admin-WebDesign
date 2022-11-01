d3c8dad93fe196cc8e789d3a91bf3e13c5e41206
Naron <naron.chen@richmond.edu>
2022-11-01 01:20:58 -0400

    established skeleton

---
+++ src/scenes/global/theme.js
@@ -0,0 +1,7 @@
+//set up to get all the colors and typography of website
+//allow light and dark
+
+import { createContext, useState, useMemo} from 'react';
+import {createTheme} from '@mui/material/styles';
+
+//color design token
