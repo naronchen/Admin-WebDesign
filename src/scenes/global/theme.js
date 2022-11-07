520f6f2c2aa640c19a360a31355b6927629f5a77
Naron <naron.chen@richmond.edu>
2022-11-06 19:48:24 -0500

    completed color theme

--- src/scenes/global/theme.js
+++
@@ -1,7 +0,0 @@
-//set up to get all the colors and typography of website
-//allow light and dark
-
-import { createContext, useState, useMemo} from 'react';
-import {createTheme} from '@mui/material/styles';
-
-//color design token
